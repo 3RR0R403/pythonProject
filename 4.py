d6ed02272a1e810ad8d54f47bce328cd2ed25c9c
3RR0R403 <dobroleg08@gmail.com>
2023-05-01 16:10:27 +0300

    new version

--- 4.py
+++ 4.py
@@ -1,1 +1,7 @@
-print("Hello World")
+name = input("Name-")
+sname = input("Surname-")
+
+if name == "Богдан" and sname == "Коваль":
+    print("Здравствуйте учитель")
+else:
+    print("Здравсвуйте", sname, name)
